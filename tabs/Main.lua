3ce779fbc17d07bbeb58eec768002f74a7f8e4eb
Zachary-Minarik-Eastman at codea-scm <Zachary-Minarik-Eastman@codea-scm.aws.mapote.com>
2016-11-02 14:51:23 +0000

    Init

---
+++ tabs/Main.lua
@@ -0,0 +1,26 @@
+-- Company Logo
+
+--Created by: Zachary Minarik-Eastman
+--Created on: Oct. 2016
+--Created for: ICS2O
+--This program displays the company logo
+
+-- Use this function to perform your initial setup
+function setup()
+    supportedOrientations(LANDSCAPE_ANY)
+    displayMode(FULLSCREEN)
+    noFill()
+    noSmooth()
+    noStroke()
+    pushStyle()
+end
+
+-- This function gets called once every frame
+function draw()
+    -- This sets a dark background color 
+    background(255, 255, 255, 255)
+
+    -- Do your drawing here
+    sprite("Project:companyLogoImage", WIDTH/2, HEIGHT/2, WIDTH, HEIGHT)
+end
+
